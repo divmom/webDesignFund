f2d63f11b43d90db72a3f48988acf77165d83041
divmom <vid.sigsoln@gmail.com>
2015-03-13 10:12:44 -0400

    Make index.html responsive - DOM breakpoint at 550px.

---
+++ toggleNav.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('#menuIcon')[0].addEventListener('click', function(e){
+		$('#changeForScreen').toggleClass('showNav');
+		$('#changeForScreen').toggleClass('lgScreen');
+	});
+});
+
